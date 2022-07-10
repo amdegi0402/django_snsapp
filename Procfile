68b84cd39322cb43d191a946db972b1c16077c4e
akira.t <planet415@gmaiwl.com>
2022-07-10 15:11:47 +0900

    firsr commit
    django snsアプリ

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn snsApp.wsgi --log-file -
---
+++ sns/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    snsimage = models.ImageField(upload_to='')
+    good = models.IntegerField(null=True, blank=True, default=1)
+    read = models.IntegerField(null=True, blank=True, default=1)
+    readText = models.TextField(null=True, blank=True, default='a')
+    
