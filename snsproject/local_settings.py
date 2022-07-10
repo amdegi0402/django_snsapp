384836bfb37b33504cda30d29f66e39e7e902c3e
akira.t <planet415@gmaiwl.com>
2022-07-10 21:12:17 +0900

    django_heroku追加6

--- snsproject/local_settings.py
+++ snsproject/local_settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku
+django_heroku.settings(locals())
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEYS = 'django-insecure-d(7!(qm7cs^r-7i!fg#&i4k-h3$ld@618hy^qr^)!j-_^52!iq' #setting.pyからコピーしたsecret_key
 
